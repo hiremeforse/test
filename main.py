9ec9166232d2a6313f20e2ffc4c144354f179ed0
Senior software engineer <punitkulkarni91@gmail.com>
2023-04-20 14:07:12 +0530

    fast api

--- main.py
+++ main.py
@@ -1,7 +1,65 @@
-from fastapi import FastAPI
-
+from typing import List
+from fastapi import FastAPI, HTTPException 
+from models import User, Gender,Role
+from uuid import uuid4,UUID
 app = FastAPI()
 
+
+db: List[User] = [
+    User(id=uuid4(),
+    firstname = "punit", 
+    lastname="Gautam",
+    gender=Gender.male,
+    roles = [Role.student]),
+
+    User(id=uuid4(),firstname = "Supriya", 
+    lastname="Kulkarni",
+    gender=Gender.male,
+    roles = [Role.student]),
+
+    User(id=uuid4(),firstname = "Alex", 
+    lastname="jamkhed",
+    gender=Gender.female,
+    roles = [Role.student]),
+    ]
+
+
+#Get Data
 @app.get("/")
-def root():
+async def root():
     return {"hello" : "world"}
+
+#Get with Db methods defined
+@app.get("/api/v1/user/")
+async def fetch_user():
+    print("this is the data")
+    return db
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id" : user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user : UUID):
+    for user in db:
+        db.remove(user)
+        return
+    raise HTTPException(
+        status_code = 404,
+        detail = f"user_id : {user_id} does not exist"
+    )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update : Userupaterequest, user_id : UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.firstname is not None:
+                user.firstname = user_update.firstname
+            if user_update.lastname is not None:
+                user.firstname = user_update.firstname
+            return
+    raise HTTPException(
+        status_code = 404,
+        detail=f"user with id {user_id} does not exists "
+    )
---
+++ models.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+from uuid import UUID,uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+    male = "admin"
+    female = "user"
+    student = "student"
+
+
+class User(BaseModel):
+    id : Optional[UUID] = uuid4()
+    firstname : str
+    middlename : Optional[str]
+    lastname : str
+    gender : Gender
+    roles : List[Role]
+
+class Userupaterequest(BaseModel):
+    firstname : Optional[str]
+    lastname : optional[str]
